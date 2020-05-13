a806678d1f58974483be0f029d66f78bd254379c
askuric <antun.skuric@outlook.com>
2020-05-13 18:41:23 +0200

    FEAT same origin problem

--- assets/js/payment.js
+++ assets/js/payment.js
@@ -157,9 +157,9 @@ async function getSecret(url = '', data = {}) {
   // Default options are marked with *
   const response = await fetch(url, {
     method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
+    mode: 'no-cors', // no-cors, *cors, same-origin
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'include', // include, *same-origin, omit
+    credentials: 'omit', // include, *same-origin, omit
     headers: {
       //'Content-Type': 'application/json'
        'Content-Type': 'application/x-www-form-urlencoded',
